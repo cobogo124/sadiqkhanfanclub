a9189246f00bcfec27ad732d6248e433ae1cd5e5
k23064919 <k23064919@kcl.ac.uk>
2026-03-31 13:36:35 +0100

    add logic to distinguish between line and stops

--- data_retrieval/nlpPipeline.py
+++ data_retrieval/nlpPipeline.py
@@ -5,14 +5,24 @@ from rdflib import Graph, URIRef, Literal, RDF, Namespace
 nlp = spacy.load("en_core_web_sm")
 LT = Namespace("http://kcl.ac.uk/ontology/london-transport#")
 
+# Standardize how URIs are generated across both scripts
+def get_uri(name):
+    # Convert "Green Park" to "green_park" for consistency
+    clean_name = name.strip().lower().replace(" ", "_")
+    return URIRef(LT[clean_name])
+
 def extract_transport_entities(text):
     doc = nlp(text)
     g = Graph()
     
     for ent in doc.ents:
-        # Custom logic to map 'GPE' or 'ORG' to TransitStops/Lines
-        if ent.label_ in ["FAC", "GPE", "ORG"]:
-            entity_uri = URIRef(LT[ent.text.replace(" ", "_")])
+        entity_uri = get_uri(ent.text)
+        
+        # Logic to distinguish between Lines and Stops
+        if "line" in ent.text.lower():
+            g.add((entity_uri, RDF.type, LT.TfLLine)) # Use TfLLine from your ontology
+            g.add((entity_uri, LT.lineName, Literal(ent.text)))
+        elif ent.label_ in ["FAC", "GPE", "ORG"]:
             g.add((entity_uri, RDF.type, LT.TransitStop))
             g.add((entity_uri, LT.stopName, Literal(ent.text)))
             
